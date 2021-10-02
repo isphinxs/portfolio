5fac22cc4c8120b7193d1ff4bdc27b3d718d63d6
Samantha Ostrowski <isphinxs@hotmail.com>
2021-10-02 16:29:53 -0400

    Add react-router-dom and draft App and Home components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h3>Hi, my name is Sam! </h3>
+        </header>
+        <body>
+          <p>About</p>
+          <p>Projects</p>
+          <p>Contact</p>
+        </body>
+      </div>
+    </Router>
   );
 }
 
