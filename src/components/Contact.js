cc6a1672e2ae90c2c6e86c7da2613e05bcf32aea
Samantha Ostrowski <isphinxs@hotmail.com>
2021-10-08 18:00:37 -0400

    Add fontawesome and start contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,17 @@ function Contact() {
                 <li><a href="https://github.com/isphinxs">GitHub</a></li>
             </ul>
             <form>
-                <p>Contact Form TBD</p>
+                <div className="title">Contact Sam</div>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <i className="fas fa-user"></i>
+                    <input type="text" name="name" id="name" placeholder="Gremelda" />
+                    <i className="fas fa-exclamation-circle failure-icon"></i>
+                    <i className="far fa-check-circle success-icon"></i>
+                    <div className="error"></div>
+                </div>
+                <div></div>
+                <div></div>
             </form>
         </div>
     )
