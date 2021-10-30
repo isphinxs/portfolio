adb0065273b919397b69ebc9ad0b119a7fcbe3bf
Samantha Ostrowski <isphinxs@hotmail.com>
2021-10-29 21:04:36 -0400

    Adjust Home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,12 @@ import resume from '../undraw_resume.svg';
 
 function Home() {
     return(
-        <div>
-            <p>I'm a full-stack software engineer experienced in JavaScript, React, and Rails, with a background in science education and program management.</p>
-            <br />
+        <div className="container">
+            <div className="content">
+                <p>I'm a full-stack software engineer experienced in JavaScript, React, and Rails, with a background in science education and program management.</p>
+                <a href={Pdf} target="_blank" rel="noreferrer">Download Sam's Resume</a>
+            </div>
             <img alt="" className="illustration" src={resume} />
-            <a href={Pdf} target="_blank" rel="noreferrer">Download Sam's Resume</a>
         </div>
     )
 }
