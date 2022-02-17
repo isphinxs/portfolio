7320deda0782d9e6b63d025ba7336fd472c8851b
Samantha Ostrowski <isphinxs@hotmail.com>
2022-02-16 20:02:38 -0500

    Refactor dropdown

--- src/components/Navbar/Dropdown.js
+++ src/components/Navbar/Dropdown.js
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { MenuItems } from './MenuItems';
 import './Dropdown.css';
 
 const Dropdown = props => {
-    const [click, setClick] = useState(false);    
-    const handleClick = () => {
-        setClick(!click);
-    }
-    
     return(
         <>
-            <ul onClick={props.closeMenu} className={click ? 'dropdown-menu clicked' : 'dropdown-menu'}>
+            <ul onClick={props.closeMenu} className='dropdown-menu'>
                 {MenuItems.map((item, index) => {
                     return (
                         <li key={index}>
-                            <Link className={item.className} to={item.path} onClick={() => {
-                                setClick(false);
-                            }}>
+                            <Link className={item.className} to={item.path} onClick={props.closeMenu}>
                                 {item.title}
                             </Link>
                         </li>
