6ac4d08d402c5007dd7449a8ab27bff5d8253f38
Samantha Ostrowski <isphinxs@hotmail.com>
2022-03-23 20:48:11 -0400

    Adjust grid on small screen

--- src/components/Projects/ProjectElements.js
+++ src/components/Projects/ProjectElements.js
@@ -28,7 +28,7 @@ export const ProjectRow = styled.div`
     grid-template-areas: ${({imgStart}) => (imgStart ? `'col2 col1'` : `'col1 col2'`)};
 
     @media screen and (max-width: 768px) {
-        grid-template-areas: ${({imgStart}) => (imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`)};
+        grid-template-areas: 'col1' 'col2';
     }
 `;
 
